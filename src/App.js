34ab47e4ca65b65f62433b0faa47f7e851e19e86
Dario Evers <darioevers@gmail.com>
2021-09-14 21:18:18 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import './scss/main.scss';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// GLOBAL IMPORTS
+import Sidenav from "./components/Sidenav";
+import Topnav from "./components/Topnav";
+import Languagebox from "./components/Languagebox";
+import Contactbox from "./components/Contactbox";
+import Notificationbox from "./components/Notificationbox";
+import Profilebox from "./components/Profilebox";
+
+// DASHBOARD IMPORTS
+import Dashboardtopbox from "./components/Dashboardtopbox";
+import Dashboardleftbotbox from "./components/Dashboardleftbotbox";
+import Dashboardmidbotbox from "./components/Dashboardmidbotbox";
+import Dashboardrightbotbox from "./components/Dashboardleftbotbox";
+
+// ABOUT IMPORTS
+// SKILLS IMPORTS
+// WORKS IMPORTS
+// CONTACT IMPORTS
+// SETTINGS IMPORTS
+// RESSOURCES IMPORTS
+// FAQ IMPORTS
+// 404 IMPORTS
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      
+      {/* GLOBAL COMPONENTS */}
+      <Sidenav />
+      <Topnav />
+      <Languagebox />
+      <Contactbox />
+      <Notificationbox />
+      <Profilebox />
+      {/* DASHBOARD COMPONENTS */}
+      <Dashboardtopbox />
+      <Dashboardleftbotbox />
+      <Dashboardmidbotbox />
+      <Dashboardrightbotbox />
+        {/* ABOUT COMPONENTS */}
+        </Router>
   );
 }
-
 export default App;
