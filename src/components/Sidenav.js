627886e5b3782256b3dd749471bd7dc7b9b3d7fe
Dario Evers <darioevers@gmail.com>
2021-09-16 13:02:37 +0200

    added buttons and button functionality

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,36 +1,61 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Sidenav = () => {
-    return (
+  return (
     <div class="sidenav_main">
+      <div class="sidenav_boxes">
         <div class="sidenav_top"></div>
         <div class="sidenav_mid">
-            <ul class="sidenav_navigation">
-                <li>
-                <Link to="/"><i class="fas fa-home"> Dashboard</i></Link>
-                </li>
-                <li>
-                <Link to="/"><i class="fas fa-home"> About</i></Link>
-                </li>
-                <li>
-                <Link to="/"><i class="fas fa-home"> Skills</i></Link>
-                </li>
-                <li>
-                <Link to="/"><i class="fas fa-home"> Works</i></Link>
-                </li>
-                <li>
-                <Link to="/"><i class="fas fa-home"> Contact</i></Link>
-                </li>
-                <li>
-                <Link to="/"><i class="fas fa-home"> Settings</i></Link>
-                </li>
-            </ul>
+          <ul class="sidenav_navigation">
+            <li>
+              <Link to="/">
+                <button>
+                  <i class="fas fa-home"> Dashboard</i>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/about">
+                <button>
+                  <i class="fas fa-home"> About</i>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/skills">
+                <button>
+                  <i class="fas fa-home"> Skills</i>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/works">
+                <button>
+                  <i class="fas fa-home"> Works</i>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact">
+                <button>
+                  <i class="fas fa-home"> Contact</i>
+                </button>
+              </Link>
+            </li>
+            <li>
+              <Link to="/settings">
+                <button>
+                  <i class="fas fa-home"> Settings</i>
+                </button>
+              </Link>
+            </li>
+          </ul>
         </div>
         <div class="sidenav_bot"></div>
-    </div>)
-}
+      </div>
+    </div>
+  );
+};
 
 export default Sidenav;
-
-{/* <Link to="/"><i class="fas fa-home"></i></Link> */}
