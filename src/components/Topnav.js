23733dc31bdf0ab7002805b17d9aea200bc37cbe
Dario Evers <darioevers@gmail.com>
2021-09-17 12:27:02 +0200

    added topnav buttons and styling

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -4,8 +4,25 @@ const Topnav = () => {
   return (
     <div className="topnav_mainbox">
       <div className="topnav_box">
-        <div className="topnav_box_left">darioevers.com</div>
-        <div className="topnav_box_right"></div>
+        <div className="topnav_box_left">
+          <div className="topnav_box_left_addressbox">
+            <h4>www.darioevers.com</h4>
+          </div>
+        </div>
+        <div className="topnav_box_right">
+          <div className="contactbox_button">
+            <i class="fas fa-envelope"></i>
+          </div>
+          <div className="notificationbox_button">
+            <i class="fas fa-bell"></i>
+          </div>
+          <div className="profilebox_button">
+            <div className="profilebox_profilepic">
+              <p>D</p>
+            </div>
+            <p>Dario Evers</p>
+          </div>
+        </div>
       </div>
     </div>
   );
